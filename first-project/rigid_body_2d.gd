22d1165dcbd92c70ddbb6f68d3e5dbec9d4c78ae
colrocker <mlezivaaris@gmail.com>
2026-08-27 19:06:54 +0300

    worked on dropoff

--- first-project/rigid_body_2d.gd
+++ first-project/rigid_body_2d.gd
@@ -9,3 +9,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+
+func _on_drop_of_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("DropOff"):
+		print("dropped")
