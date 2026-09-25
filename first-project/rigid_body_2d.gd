ccc6445d2a29ee4f67b248a73fa9a6c57a6cee5f
colrocker <mlezivaaris@gmail.com>
2026-09-25 17:27:36 +0300

    merjii

--- first-project/rigid_body_2d.gd
+++ first-project/rigid_body_2d.gd
@@ -1,10 +1,10 @@
 extends RigidBody2D
 
+@onready var Sprite: Sprite2D = $Icon
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,10 +13,10 @@ func _process(delta: float) -> void:
 
 
 func _on_drop_of_area_body_entered(body: Node2D) -> void:
-	if (body.name == "moveable cube"):
+	if body.is_in_group("Rigidbody"): 
 		print ("enterd")
 
 
 func _on_drop_of_area_body_exited(body: Node2D) -> void:
-	if (body.name == "moveable cube"):
+	if body.is_in_group("Rigidbody"):
 		print ("left")
